b120ddb148b2b5cdb0f714f193dacb919de8d29f
peter <peter@peter-ThinkPad-X201-Tablet.(none)>
2013-08-05 16:01:06 +0100

    First proper working commit, albeit only locally

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ var express = require('express')
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
-var client = dgram.createSocket('udp4');
-var client_mutex = 0;
-var sendPort = 8080;
+var sendPort = 4002;
 var app = express();
 
 // all environments
@@ -56,41 +54,26 @@ http.createServer(app).listen(app.get('port'), function(){
 server.on("message",function(msg,rinfo){
     console.log("Got UDP Message: " + msg);
     var str = msg.toString();
-    if(str.indexOf("get")===0){
+    if(str.indexOf("get")!=-1){
 	var num = parseInt(str.substring(4),10);
 	sensors.showVal({params: {num : num}},
 			{
 			    send: function(str){
-				if(!client_mutex){
-				    client_mutex = 1;
-				    client = dgram.createSocket("udp4");
 				    var buf = new Buffer(str);
-				    //buf.write(str,'ascii');
-				    //console.log(str);
 				    var dest = sensors.getWatcher(num);
 
-				    console.log("Dest: " + dest + ", rinfo: " + rinfo.address);
-				    //console.log("Going to send number " + num + " to: " + dest);
-				    //console.log("Str: " + str + ",Buffer: " + buf.toString('ascii',0,24));
-				    /*client.send(
-				      buf,0,str.length,4000,rinfo.address,function(err,bytes){
-				      //new Buffer("Hello"),0,5,4001,dest,function(err,bytes){
-				      if(err)
-				      console.log(err);
-				      else console.log("sent UDP Response: " + buf);
-				      });*/
-				    client.send(
-					buf,0,buf.length,sendPort,dest);//,function(err,bytes){
-				    //new Buffer("Hello"),0,5,4001,dest,function(err,bytes){
-				    /*if(err)
-				      console.log(err);
-				      else console.log("sent UDP Response: " + buf + " bytes long to: " + dest);
-				      });*/
-				    client.send(
-					buf,0,buf.length,sendPort,rinfo.address);
-				    client.close();
-				    client_mutex = 0;
-				}
+				    //console.log("Dest: " + dest + ", rinfo: " + rinfo.address);
+				    //console.log("Str: " + str + ", Buf: " + buf.toString());
+				    
+				    server.send(buf,0,buf.length,sendPort,rinfo.address,function(err,bytes){
+						console.log("Sent");
+						if(err)
+							console.log(err.toString);
+						//else if(bytes){
+						//	console.log("bytes sent: " + bytes);
+						}
+				    
+				    );
 			    }
 			}
 		       );
